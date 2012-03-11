17fc52fc6e833cdba473738c3704392f686eb24c
ravthan <ravthan@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-11 16:55:26 +0000

    adding a file

---
+++ Plist Navigator/src/org/tbrt/plist/PlistNavigator.java
@@ -0,0 +1,233 @@
+package org.tbrt.plist;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.BorderLayout;
+import javax.swing.JTree;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PlistNavigator {
+
+	private JFrame frmPlistNavigator;
+	private InvestigationTree investigationTree;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PlistNavigator window = new PlistNavigator();
+					window.frmPlistNavigator.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PlistNavigator() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPlistNavigator = new JFrame();
+		frmPlistNavigator.setTitle("Plist Investigator");
+		frmPlistNavigator.setBounds(50, 50, 600, 500);
+		frmPlistNavigator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmPlistNavigator.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnSearch = new JMenu("Edit");
+		menuBar.add(mnSearch);
+		
+		JMenuItem mntmSearchEvidence = new JMenuItem("Search File");
+		mnSearch.add(mntmSearchEvidence);
+		
+		JMenuItem mntmSearchAll = new JMenuItem("Search All");
+		mnSearch.add(mntmSearchAll);
+		
+		JMenu mnInvestigation = new JMenu("Investigation");
+		menuBar.add(mnInvestigation);
+			
+		JMenuItem mntmNewProject = new JMenuItem("New");
+		mnInvestigation.add(mntmNewProject);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnInvestigation.add(mntmOpen);		
+		
+		JMenuItem mntmClose = new JMenuItem("Close");
+		mnInvestigation.add(mntmClose);
+		
+		JMenuItem mntmDelete = new JMenuItem("Delete");
+		mnInvestigation.add(mntmDelete);
+		
+		JMenuItem mntmProperties = new JMenuItem("Properties");
+		mnInvestigation.add(mntmProperties);		
+		
+		JMenu mnEdit = new JMenu("Evidence");
+		menuBar.add(mnEdit);
+		
+		JMenuItem mntmAddEvidence = new JMenuItem("Add Evidence");
+		mnEdit.add(mntmAddEvidence);
+		
+		JMenuItem mntmAddAllEvidence = new JMenuItem("Add All Evidence");
+		mnEdit.add(mntmAddAllEvidence);
+		
+		JMenuItem mntmRemoveEvidence = new JMenuItem("Remove Evidence");
+		mnEdit.add(mntmRemoveEvidence);
+		
+		JMenuItem mntmRemoveAllEvidence = new JMenuItem("Remove All Evidence");
+		mnEdit.add(mntmRemoveAllEvidence);
+		
+		JMenuItem mntmVerifyEvidence = new JMenuItem("Verify Evidence");
+		mnEdit.add(mntmVerifyEvidence);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		frmPlistNavigator.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		//===================================================================
+		// Create the toolbar
+		//===================================================================
+		JToolBar toolBar = new JToolBar();
+		frmPlistNavigator.getContentPane().add(toolBar, BorderLayout.NORTH);
+		
+		//-------------------------------------------------------------------
+		// Create the "Add" toolbar button
+		//-------------------------------------------------------------------
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO
+				investigationTree.addObject("NEW NODE");
+			}
+		});
+		btnAdd.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		toolBar.add(btnAdd);
+		
+		//-------------------------------------------------------------------
+		// Create the "Remove" toolbar button
+		//-------------------------------------------------------------------
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO
+				investigationTree.removeSelectedNode();
+			}
+		});
+		btnRemove.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		toolBar.add(btnRemove);
+		
+		//-------------------------------------------------------------------
+		// Create the "Clear" toolbar button
+		//-------------------------------------------------------------------
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO
+				investigationTree.removeAllNodes();
+			}
+		});
+		btnClear.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		toolBar.add(btnClear);
+		
+		//-------------------------------------------------------------------
+		// TODO - Add more buttons
+		//-------------------------------------------------------------------
+		
+		//===================================================================
+		// Create the status bar at the bottom of the GUI
+		//===================================================================
+		JToolBar statusBar = new JToolBar();
+		frmPlistNavigator.getContentPane().add(statusBar, BorderLayout.SOUTH);
+		
+		JLabel lblMessages = new JLabel("Messages.....");
+		statusBar.add(lblMessages);
+		
+		JPanel panel = new JPanel();
+		frmPlistNavigator.getContentPane().add(panel, BorderLayout.CENTER);
+		SpringLayout sl_panel = new SpringLayout();
+		panel.setLayout(sl_panel);
+		
+		//===================================================================
+		// Create tree
+		//===================================================================
+		investigationTree = new InvestigationTree();
+		sl_panel.putConstraint(SpringLayout.NORTH, investigationTree, 10, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, investigationTree, 10, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, investigationTree, -10, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, investigationTree, -443, SpringLayout.EAST, panel);
+		populateTree();
+		panel.add(investigationTree);		
+		
+		//===================================================================
+		// Tab panels
+		//===================================================================
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		sl_panel.putConstraint(SpringLayout.NORTH, tabbedPane, 10, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, tabbedPane, 6, SpringLayout.EAST, investigationTree);
+		sl_panel.putConstraint(SpringLayout.SOUTH, tabbedPane, 383, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, tabbedPane, -10, SpringLayout.EAST, panel);
+		panel.add(tabbedPane);
+		
+	}
+	
+	private void populateTree()
+	{
+		InvestigationNode node = new InvestigationNode("Investigation", "MyInvestigation");
+		
+		InvestigationNode node1 = new InvestigationNode("EvidenceItems", "Evidence");
+		InvestigationNode node2 = new InvestigationNode("Notes", "Notes");
+		
+		InvestigationNode evid1 = new InvestigationNode("EvidenceItem", "Plist1");
+		InvestigationNode note1 = new InvestigationNode("Note", "Finding#1");
+		
+		DefaultMutableTreeNode p  = investigationTree.addObject(null, node);
+		
+		DefaultMutableTreeNode p1 = investigationTree.addObject(p, node1);
+		investigationTree.addObject(p1, evid1);
+		
+		DefaultMutableTreeNode p2 = investigationTree.addObject(p, node2);
+		investigationTree.addObject(p2, note1);
+		
+		return;
+	}
+	
+}
