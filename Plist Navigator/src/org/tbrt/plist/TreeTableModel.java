cfcb8b0f3a6df7535c99dadc6988c058f38e8eac
tbrt.tom@gmail.com <tbrt.tom@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-11 18:26:02 +0000

    Added files

---
+++ Plist Navigator/src/org/tbrt/plist/TreeTableModel.java
@@ -0,0 +1,14 @@
+package org.tbrt.plist;
+
+import javax.swing.tree.TreeModel;
+
+public interface TreeTableModel extends TreeModel
+{
+    public int getColumnCount();
+    public Object getValueAt(Object node, int column);
+    public String getColumnName(int column);
+    public Class getColumnClass(int column);
+    public void setValueAt(Object aValue, Object node, int column);
+    public boolean isCellEditable(Object node, int column);
+}
+
---
+++ Plist Navigator/src/org/tbrt/plist/TreeTableModelAdapter.java
@@ -0,0 +1,61 @@
+package org.tbrt.plist;
+
+import javax.swing.JTree;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.tree.TreePath;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
+
+public class TreeTableModelAdapter extends AbstractTableModel
+{
+     TreeTableModel m_model;
+     JTree m_tree;
+
+    public TreeTableModelAdapter(TreeTableModel model, JTree tree) {
+        this.m_tree = tree;
+        this.m_model = model;
+
+	tree.addTreeExpansionListener(new TreeExpansionListener() {
+	    public void treeExpanded(TreeExpansionEvent e) {  
+	      fireTableDataChanged(); 
+	    }
+            public void treeCollapsed(TreeExpansionEvent e) {  
+	      fireTableDataChanged(); 
+	    }
+	});
+    }
+
+    public int getColumnCount() {
+	    return m_model.getColumnCount();
+    }
+    
+    public Object getValueAt(int rowNum, int columnNum) {
+	    return m_model.getValueAt(nodeForRow(rowNum), columnNum);
+    }
+    
+    public String getColumnName(int columnNum) {
+	    return m_model.getColumnName(columnNum);
+    }
+
+    public Class getColumnClass(int columnNum) {
+	    return m_model.getColumnClass(columnNum);
+    }
+
+    public int getRowCount() {
+	    return m_tree.getRowCount();
+    }
+
+    protected Object nodeForRow(int rowNum) {
+	    TreePath treePath = m_tree.getPathForRow(rowNum);
+	    return treePath.getLastPathComponent();         
+    }
+
+    public void setValueAt(Object obj, int rowNum, int columnNum) {
+    	m_model.setValueAt(obj, nodeForRow(rowNum), columnNum);
+    }
+
+    public boolean isCellEditable(int rowNum, int columnNum) {
+         return m_model.isCellEditable(nodeForRow(rowNum), columnNum); 
+    }
+}
+
