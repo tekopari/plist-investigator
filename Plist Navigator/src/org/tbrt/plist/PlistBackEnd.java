add8c8e084e9d8a36b30418f733f4775ddf27fda
tbrt.tom@gmail.com <tbrt.tom@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-25 02:34:49 +0000

    Parse command line args

--- Plist Navigator/src/org/tbrt/plist/PlistBackEnd.java
+++ Plist Navigator/src/org/tbrt/plist/PlistBackEnd.java
@@ -31,40 +31,198 @@ import com.dd.plist.*;
 
 
 public class PlistBackEnd {
+	private static void usage() {
+		System.out.println("USAGE: tbrtplist.exe –i <plistfile> -o <outputfile> -t <xml | pdf | bin>");
+		System.out.println();
+		return;
+	}
+	
     public static void main(String args[])
     {
-     try
-       {
-        // Open the file that is the first
-        // command line parameter
-        //FileInputStream fstream = new
-        //FileInputStream("C:/tmp/plistfile.plist");
-        // Convert our input stream to a
-        // DataInputStream
-        //DataInputStream in =  new DataInputStream(fstream);
-        // Continue to read lines while
-        // there are still some left to read
-        //byte b[] = new byte[10];
-        //in.read(b);
+    	//-------------------------------------------------------------------
+    	// Check for the correct number of command line arguments
+    	//-------------------------------------------------------------------
+    	if (args.length != 6) {
+    		usage();
+    		System.exit(1);
+    	}
+    	
+    	//-------------------------------------------------------------------
+    	// Parse the command line arguments
+    	//-------------------------------------------------------------------
+    	String inputFile  = "";
+    	String outputFile = "";
+    	boolean xmlOutput = false;
+    	boolean pdfOutput = false;
+    	boolean binOutput = false;
+    	boolean processingInputOption  = false;
+    	boolean processingOutputOption = false; 
+    	boolean processingOutputType   = false;
+        for (String s: args) {
+        	
+        	//---------------------------------------------------------------
+        	// Ignore null or empty strings
+        	//---------------------------------------------------------------
+        	if(s == null || s.equals("")) {
+        		continue;
+        	}
+        	//---------------------------------------------------------------
+        	// User specified the input file option
+        	//---------------------------------------------------------------   	
+        	else if(s.equals("-i")) {
+        		if(processingInputOption ||
+             	   processingOutputOption || 
+             	   processingOutputType) {
+             		usage();
+             		System.exit(1);
+             	}
+        		inputFile = "";
+        		processingInputOption = true;
+        	}
+        	//---------------------------------------------------------------
+        	// User specified the input file option
+        	//---------------------------------------------------------------   	
+        	else if(s.equals("-o")) {
+        		if(processingInputOption ||
+        		   processingOutputOption || 
+        		   processingOutputType) {
+        			usage();
+        			System.exit(1);
+        		}
+        		outputFile = "";
+        		processingOutputOption = true;
+        	}
+        	//---------------------------------------------------------------
+        	// User specified the input file option
+        	//---------------------------------------------------------------   	
+        	else if(s.equals("-t")) {
+        		if(processingInputOption ||
+                   processingOutputOption || 
+             	   processingOutputType) {
+             		usage();
+             		System.exit(1);
+             	}
+            	xmlOutput = false;
+            	pdfOutput = false;
+            	binOutput = false;
+            	processingOutputType = true;
+        	}
+        	//---------------------------------------------------------------
+        	// Looking for the output filename
+        	//---------------------------------------------------------------   	
+        	else if(processingInputOption) {
+        		inputFile = s;
+        		processingInputOption = false;
+        	}
+        	//---------------------------------------------------------------
+        	// Looking for the output filename
+        	//---------------------------------------------------------------   	
+        	else if(processingOutputOption) {
+        		outputFile = s;
+        		processingOutputOption = false;
+        	}
+        	//---------------------------------------------------------------
+        	// Looking for a valid output type
+        	//---------------------------------------------------------------   	
+        	else if(processingOutputType) {
+        		if(s.equals("xml")) {
+        			xmlOutput = true;
+        			processingOutputType = false;
+        		}
+        		else if(s.equals("pdf")) {
+        			pdfOutput = true; 
+        			processingOutputType = false;
+        		}
+        		else if(s.equals("bin")) {
+        			binOutput = true;
+        			processingOutputType = false;
+        		}
+        		else {
+        			usage();
+        			System.exit(1);
+        		}
+        	} 
+        }
+        
+        //-------------------------------------------------------------------
+        // Check if specified all the options and parsing completed
+        //-------------------------------------------------------------------
+    	if(inputFile.equals("") ||
+    	   outputFile.equals("") ||
+    	   (!xmlOutput && !pdfOutput && !binOutput) ||
+    	   (xmlOutput && pdfOutput) ||
+    	   (xmlOutput && binOutput) ||
+    	   (pdfOutput && binOutput) ||
+    	   processingInputOption  ||
+    	   processingOutputOption || 
+    	   processingOutputType) {
+			usage();
+			System.exit(1);	
+    	}
+    	
+        //-------------------------------------------------------------------
+        // Check to see if the input file exists
+        //-------------------------------------------------------------------
+        File in = null;
         try {
-           File file = new File("C:/tmp/plistfile.plist");
-           // File file = new File("C:/tmp/lab3.tgz");
-           //using opensource parser, please see java files in com.dd.plist for more information.
-            NSDictionary rootDict = (NSDictionary)PropertyListParser.parse(file);
-             }
-              catch (Exception e)
-             {
-               System.err.println("Cannot use the PropertyListParser");
-             }
-          //System.out.write(b);
-
-          // Print file line to screen
-            System.out.println ("TBRT team is testing\n");
-            // in.close();
-          }
+        	in = new File(inputFile);   	
+        	if(!in.exists()) {
+        		System.err.println("Specified input file [" 
+        	                      + inputFile 
+        	                      + "] does not exist");
+        		System.exit(1);
+        	}
+        }
+        catch (Exception e) {
+        	System.err.println("Error occurred while check if input file exists");
+        	System.err.println("Exception details:");
+        	System.err.println(e);
+        	System.exit(1);
+        }
+        
+        //-------------------------------------------------------------------
+        // Parse the input file
+        //-------------------------------------------------------------------
+        NSDictionary rootDict = null;
+        try
+        {   
+        	rootDict = (NSDictionary)PropertyListParser.parse(in);
+        }
         catch (Exception e)
-          {
-              System.err.println("File input error");
-           }
+        {
+        	System.err.println("Error occurred while parsing input file.");
+        	System.err.println("Exception details:");
+        	System.err.println(e);
+        	System.exit(1);
+        }
+        
+        //-------------------------------------------------------------------
+        // Write the output file based on the user specified type
+        //-------------------------------------------------------------------
+        try
+        { 
+        	// TODO: what if user wants to send the output to stdout
+        	File out = new File(outputFile);
+        	
+        	if(xmlOutput) {
+        		PropertyListParser.saveAsXML(rootDict, out);
+        	}
+        	else if(pdfOutput) {
+        		// TODO:  will have to save it as xml then convert to pdf
+        		PropertyListParser.saveAsXML(rootDict, out);
+        	}
+        	else if(binOutput) {
+        		PropertyListParser.saveAsBinary(rootDict, out);
+        	}
+        }
+        catch (Exception e)
+        {
+        	System.err.println("Error occurred creating output file.");
+        	System.err.println("Exception details:");
+        	System.err.println(e);
+        	System.exit(1);
+        }
+        
+    	System.exit(0);
     }
 }
