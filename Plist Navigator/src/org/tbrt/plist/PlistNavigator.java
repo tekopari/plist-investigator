04dc7b12d3d55f51f548468b67cb4935e04c3bbe
todddchu@gmail.com <todddchu@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-28 19:41:25 +0000

    Moved the adding Plist operation to InvestigationTree.java

--- Plist Navigator/src/org/tbrt/plist/PlistNavigator.java
+++ Plist Navigator/src/org/tbrt/plist/PlistNavigator.java
@@ -45,6 +45,9 @@ public class PlistNavigator {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		String pwd = System.getProperties().getProperty("user.home");
+		System.out.print("TC:"+pwd);
+		
  		//-------------------------------------------------------------------
 		// Check the usage:  
 		// - The INSTALL_PATH identifies the path the tool was installed in.
@@ -74,12 +77,12 @@ public class PlistNavigator {
 			System.err.println("Error: " + e.toString());
  			System.exit(1);		
 		}
-		
+	
 		//-------------------------------------------------------------------
         // Initialize the application	
 		//-------------------------------------------------------------------
 		Configuration.initConfiguration(installPath);
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -128,7 +131,6 @@ public class PlistNavigator {
         		String s = (String)JOptionPane.showInputDialog(
         				frame,
         				"Type the investigation folder name:\n");
-        		//If a string was returned, say so.
         		if ((s != null) && (s.length() > 0)) {
         			InvestigationNode node = new InvestigationNode("Investigation", s);
         			InvestigationNode notes = new InvestigationNode("Notes", "Notes");		
@@ -138,69 +140,6 @@ public class PlistNavigator {
         	}
         });
 
-        //Temporary Menu Item
-		JMenuItem mntmTempPList = new JMenuItem("Add PList (Temporary Item)");
-		mnInvestigation.add(mntmTempPList);
-		
-///////// SECTION BELOW SHOULD BE MOVED TO FILE InvestigationTree.java //////////
-		
-        // TBRT addition
-        mntmTempPList.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-        		// TODO
-        		// Create a dialog box to get the name of the jtree node
-        		Component frame = null;
-        		String s = (String)JOptionPane.showInputDialog(
-        				frame,
-        				"Type plist file path:\n");
-        		//If a string was returned, say so.
-        		if ((s != null) && (s.length() > 0)) {
-        			setLabel("Typed String... " + s + "!");
-        			
-        			InvestigationNode evid = new InvestigationNode("EvidenceItems", s);
-        			InvestigationNode notes = new InvestigationNode("Notes", "Notes");
-        			DefaultMutableTreeNode t  = investigationTree.addObject(evid);
-        			investigationTree.addObject(t, notes);
-        			
-        			//investigationTree.addObject(s);	
-        			
-        			PlistTreeTable p = new PlistTreeTable(s);
-        			
-        			//bew, initial code to create directory per plist file.
-        			// first thing is to remove the .plist extension.
-        			// TODO: need to figure out how to remove the C:\dirname, so that we can
-        			//   just use the filename, and then add our own path name.  I.e. We will 
-        			//   not usually be creating the directoy in same location as where plist
-        			//   file may be.
-        			// TODO: Add additional error check, ie. what if dir exists, or invalid name.
-        			// TODO: Use the final code for getting filename, to also use when displaying
-        			//       on the GUI.
-        			s = s.substring(0, s.lastIndexOf('.'));
-        			//File f = new File("C:" + File.separator + "temp" + File.separator + s);
-        			File f = new File(s);
-        			try{
-        				f.mkdir();
-        				} catch (Exception e) {
-        					e.printStackTrace(); //TODO: Add code to handle displaying correct error codes
-        					}
-        			//bew, end add code
-        			}  else {
-        				
-        				//If you're here, the return value was null/empty.
-        				setLabel("Come on, finish the sentence!");
-        				}
-        		
-                                        
-        	}
-
-        	private void setLabel(String string) {
-        		// TODO Auto-generated method stub                                       
-        	}
-        });
-        
-///////// SECTION ABOVE SHOULD BE MOVED TO FILE InvestigationTree.java //////////
-        
-		
 		JMenuItem mntmConfig = new JMenuItem("Settings");
 		mntmConfig.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -274,7 +213,6 @@ public class PlistNavigator {
 		sl_panel.putConstraint(SpringLayout.WEST, investigationTree, 10, SpringLayout.WEST, panel);
 		sl_panel.putConstraint(SpringLayout.SOUTH, investigationTree, -10, SpringLayout.SOUTH, panel);
 		sl_panel.putConstraint(SpringLayout.EAST, investigationTree, -443, SpringLayout.EAST, panel);
-		//TC populateInvesTree();
 		panel.add(investigationTree);		
 	
 		//===================================================================
@@ -287,24 +225,4 @@ public class PlistNavigator {
 		sl_panel.putConstraint(SpringLayout.EAST, tabbedPane, -10, SpringLayout.EAST, panel);
 		panel.add(tabbedPane);
 	}
-	
-	//TC private void populateTree()
-	//TC	{
-	//TC		InvestigationNode node = new InvestigationNode("Investigation", "MyInvestigation");		
-	//TC		InvestigationNode node1 = new InvestigationNode("EvidenceItems", "Evidence");
-	//TC		InvestigationNode node2 = new InvestigationNode("Notes", "Notes");
-		
-	//TC		InvestigationNode evid1 = new InvestigationNode("EvidenceItem", "Plist1");
-	//TC		InvestigationNode note1 = new InvestigationNode("Note", "Finding#1");
-		
-	//TC		DefaultMutableTreeNode p  = investigationTree.addObject(null, node);
-		
-	//TC		DefaultMutableTreeNode p1 = investigationTree.addObject(p, node1);
-	//TC		investigationTree.addObject(p1, evid1);
-		
-	//TC		DefaultMutableTreeNode p2 = investigationTree.addObject(p, node2);
-	//TC		investigationTree.addObject(p2, note1);
-		
-	//TC		return;
-	//TC	}
 }
