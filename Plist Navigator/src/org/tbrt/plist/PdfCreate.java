7fae77033ef00a6915a8322a7b3940cb9472a343
ravthan@gmail.com <ravthan@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-28 23:50:43 +0000

    Added in the comment of the weblinks that should be useful reference.

--- Plist Navigator/src/org/tbrt/plist/PdfCreate.java
+++ Plist Navigator/src/org/tbrt/plist/PdfCreate.java
@@ -3,6 +3,9 @@ package org.tbrt.plist;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
+// http://pdfbox.apache.org/download.html
+// http://pdfbox.apache.org/userguide/cookbook.html
+
 public class PdfCreate {
 
 	public void foo(){
