5df1f218001383e3f5a8b4956987c67c3a06b907
ravthan@gmail.com <ravthan@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-28 23:41:54 +0000

    Adding the components for pdf creation

---
+++ Plist Navigator/src/org/tbrt/plist/PdfCreate.java
@@ -0,0 +1,11 @@
+package org.tbrt.plist;
+
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+public class PdfCreate {
+
+	public void foo(){
+		PDFont font = PDType1Font.HELVETICA_BOLD;
+	}
+}
