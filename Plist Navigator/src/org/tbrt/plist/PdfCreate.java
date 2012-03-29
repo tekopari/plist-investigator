dc87c867870594811018d5b9786ae3822e96a0a0
ravthan@gmail.com <ravthan@gmail.com@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-03-29 02:57:44 +0000

    pdf test program

--- Plist Navigator/src/org/tbrt/plist/PdfCreate.java
+++ Plist Navigator/src/org/tbrt/plist/PdfCreate.java
@@ -1,5 +1,11 @@
 package org.tbrt.plist;
 
+import java.io.IOException;
+
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
@@ -11,5 +17,36 @@ public class PdfCreate {
 
 	public void foo(){
 		PDFont font = PDType1Font.HELVETICA_BOLD;
+	}
+	
+	public void testcreate() throws IOException, COSVisitorException {
+		// the document
+		PDDocument doc = null;
+		try
+		 {
+			doc = new PDDocument();
+		     
+		                  PDPage page = new PDPage();
+		                  doc.addPage( page );
+		                  PDFont font = PDType1Font.HELVETICA_BOLD;
+		      
+		                  PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+		                  contentStream.beginText();
+		                  contentStream.setFont( font, 12 );
+		                  contentStream.moveTextPositionByAmount( 100, 700 );
+		                  String message = "Test by TBRT";
+						contentStream.drawString( message );
+		                  contentStream.endText();
+		                  contentStream.close();
+		                  String file = "C:/tmp/tbrt.pdf";
+						doc.save( file );
+		              }
+		 finally
+		  {
+			 if( doc != null )
+		     	{
+				 	doc.close();
+		        }
+		  }
 	}
 }
