d7109ced11bec4b207129135a2e24795027ef762
heyyoulive <heyyoulive@9c5e4973-892b-3f7c-3d35-f22be7d4288e>
2012-04-05 16:40:49 +0000

    Update code to use TBRT.png file within project.

--- Plist Navigator/src/org/tbrt/plist/DisplayImagePanel.java
+++ Plist Navigator/src/org/tbrt/plist/DisplayImagePanel.java
@@ -5,16 +5,27 @@ import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
 import javax.imageio.ImageIO;
 
 
 public class DisplayImagePanel extends JPanel{
 
     private BufferedImage displayImage;
+    private String path = "images/TBRT.png";
 
     public DisplayImagePanel() {
        try {                
-          displayImage = ImageIO.read(new File("c:\\tmp\\TBRT.png"));
+          URL imageURL = DisplayImagePanel.class.getResource(path); 
+          if (imageURL != null)
+          {
+        	  displayImage = ImageIO.read(imageURL);
+          }
+          else
+          {
+         	  System.err.println("Couldn't find file: " + path);  
+          }
        } catch (IOException ex) {
           System.out.println("Display File not found!");
        }
